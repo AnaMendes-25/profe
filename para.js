9a312b0aaf67afcbd85110cd8b9f76d3d2b0a570
AnaMendes-25 <anaclaramendes2501@gmail.com>
2025-04-29 22:12:53 -0300

    Add files via upload

---
+++ para.js
@@ -0,0 +1,27 @@
+let x = parseFloat(prompt("Digite um valor"))
+
+
+if (x % 2 ==0){
+    console.log("Esse número é par")
+} else {
+    console.log("Esse número é impar")
+}
+
+for (var i =0; i<=100; i+=+50) {
+    console.log(i)
+}
+
+console.log("Números indo de 50 em 50")
+
+for (let i = 0; i <= 50; i += 5) {
+    console.log(i);
+  }
+
+  console.log("Números indo de 5 em 5")
+
+  for (let i = 100; i >= 0; i -= 5) {
+    console.log(i);  //de trás pra frente
+  }
+
+  console.log("Números voltando de 5 em 5")
+  
